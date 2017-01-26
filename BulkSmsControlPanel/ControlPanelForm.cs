64e63f1efadc44d1bfed2d87b940a2fd08c25e3e
Ariyan Khan <ariyankhan46@gmail.com>
2017-01-26 14:08:55 +0530

    Update

--- BulkSmsControlPanel/ControlPanelForm.cs
+++ BulkSmsControlPanel/ControlPanelForm.cs
@@ -13,6 +13,8 @@ namespace BulkSmsControlPanel
 {
     public partial class ControlPanelForm : Form
     {
+        private int MIN_ROWS_IN_DATAGRIDVIEW = 30;
+
         public ControlPanelForm()
         {
             InitializeComponent();
@@ -43,6 +45,104 @@ namespace BulkSmsControlPanel
             }
         }
 
+        public static Dictionary<String, int> GetDataGridViewColumnsWidthLocalReport()
+        {
+            Dictionary<String, int> outs = new Dictionary<String, int>();
+            outs["ID"] = 200;
+            outs["Mobile Number"] =200;
+            outs["Message"] = 200;
+            outs["Status"] = 200;
+            return outs;
+        }
+
+        public static Dictionary<String, int> GetDataGridViewColumnsWidthOnlineReport()
+        {
+            Dictionary<String, int> outs = new Dictionary<String, int>();
+            outs["Mobile Number"] = 200;
+            outs["Date"] = 200;
+            outs["Time"] = 200;
+            outs["Status"] = 200;
+            outs["Request ID"] = 200;
+            return outs;
+        }
+
+        public void InitializeDataGridviewLocalReport()
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                Dictionary<String, int> all_fields = GetDataGridViewColumnsWidthLocalReport();
+                dataGridView1.ColumnCount = all_fields.Count;
+
+                for (int i = 0; i < all_fields.Keys.Count; i++)
+                {
+                    dataGridView1.Columns[i].Name = all_fields.Keys.ElementAt(i);
+                    dataGridView1.Columns[i].Width = all_fields[all_fields.Keys.ElementAt(i)];
+                    dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                }
+
+                setupGridView();
+
+            }
+            catch (Exception exp)
+            {
+                Utils.Log(exp);
+            }
+        }
+
+        public void InitializeDataGridviewOnlineReport()
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+
+                Dictionary<String, int> all_fields = GetDataGridViewColumnsWidthOnlineReport();
+                dataGridView1.ColumnCount = all_fields.Count;
+
+                for (int i = 0; i < all_fields.Keys.Count; i++)
+                {
+                    dataGridView1.Columns[i].Name = all_fields.Keys.ElementAt(i);
+                    dataGridView1.Columns[i].Width = all_fields[all_fields.Keys.ElementAt(i)];
+                    dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                }
+
+                setupGridView();
+
+            }
+            catch (Exception exp)
+            {
+                Utils.Log(exp);
+            }
+        }
+
+        private void setupGridView()
+        {
+            if (dataGridView1.RowCount < MIN_ROWS_IN_DATAGRIDVIEW)
+            {
+                dataGridView1.Rows.Add(MIN_ROWS_IN_DATAGRIDVIEW - dataGridView1.RowCount);
+            }
+
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(235, 235, 235);
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(227, 255, 246);
+                }
+                else
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(255, 255, 255);
+                }
+            }
+
+        }
+
+
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
@@ -56,9 +156,12 @@ namespace BulkSmsControlPanel
                 e.Cancel = true;
                 return;
             }
-            LoginForm form = (LoginForm)this.Owner;
-            form.textBox2.Text = "";
-            form.Show();
+            if(this.Owner != null)
+            {
+                LoginForm form = (LoginForm)this.Owner;
+                form.textBox2.Text = "";
+                form.Show();
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
--- BulkSmsControlPanel/Program.cs
+++ BulkSmsControlPanel/Program.cs
@@ -18,7 +18,7 @@ namespace BulkSmsControlPanel
             test();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new  ControlPanelForm());
         }
 
         public static void test()
